274c743f5d9df3b4667f67713a393392016775ff
Laadydee <doyinsolabello004@gmail.com>
2026-07-24 11:26:37 +0100

    Build a Password Generator App

--- Week5/Day26UnderstandingModules/func.js
+++ Week5/Day26UnderstandingModules/func.js
@@ -13,4 +13,21 @@ function uniteUnique(){
   return arr
 }
 console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]))
-//  [1, 3, 2, 5, 4]
+
+
+
+// Build a Password Generator App
+
+function generatePassword(passwordLength){
+  let randomPassword = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"
+  let pass = "";
+  for(let i =  0; i < passwordLength; i++){
+  let passwordIndex = Math.floor(Math.random()*randomPassword.length)
+ let character = randomPassword[passwordIndex]
+  pass += character
+  }
+ 
+ return pass 
+}
+let password = generatePassword(8)
+ console.log(`Generated password: ${password}`)
