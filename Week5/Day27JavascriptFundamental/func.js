31c1af54bcb96843329d16e9ae031fcb40cb18c9
Laadydee <doyinsolabello004@gmail.com>
2026-07-25 14:03:24 +0100

    Implement an Element Skipper

--- Week5/Day27JavascriptFundamental/func.js
+++ Week5/Day27JavascriptFundamental/func.js
@@ -79,3 +79,20 @@ function sumFibs(number) {
 }
 console.log(sumFibs(1000))
  
+
+
+// Implement an Element Skipper 
+
+function dropElements(arr,func){
+  for(let i = 0; i < arr.length ; i++){let n = func(arr[i])
+
+    if(n){
+     return (arr.slice([i]))    
+     }
+  } 
+return []
+}
+
+console.log(dropElements([1, 2, 3, 4], function(n) {return n >= 3;}))
+console.log(dropElements([0, 1, 0, 1], function(n) {return n === 1;}))
+// [3, 4]
